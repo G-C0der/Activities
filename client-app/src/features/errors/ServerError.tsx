47298a12d45e033c038534929b007ec781293b16
G-C0der <31240102+G-C0der@users.noreply.github.com>
2021-04-23 11:20:13 +0200

    End section 11 - Forms (Form Validation, Reusable fields, Dates optimized)

--- client-app/src/features/errors/ServerError.tsx
+++ client-app/src/features/errors/ServerError.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useStore } from '../../app/stores/store';
+import {useStore} from '../../app/stores/store';
 import {Container, Header, Segment} from "semantic-ui-react";
-import { observer } from 'mobx-react-lite';
-import {toJS} from "mobx";
+import {observer} from 'mobx-react-lite';
 
 export default observer(function ServerError() {
     const {commonStore} = useStore();
 
     return (
         <Container>
-            <Header as='h1' content='Server Error' />
-            <Header sub as='h5' color='red' content={commonStore.error?.message} />
+            <Header as='h1' content='Server Error'/>
+            <Header sub as='h5' color='red' content={commonStore.error?.message}/>
             {commonStore.error?.details && (
                 <Segment>
-                    <Header as='h4' content='Stack trace' color='teal' />
+                    <Header as='h4' content='Stack trace' color='teal'/>
                     <code style={{marginTop: 10}}>{commonStore.error.details}</code>
                 </Segment>
             )}
