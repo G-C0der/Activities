1cb11b2758eaebd6eb3d301db74bdc8be08dc406
G-C0der <31240102+G-C0der@users.noreply.github.com>
2021-05-12 14:19:40 +0200

    Filtering & Sorting

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Application.Activities
     {
         public class Query : IRequest<Result<PagedList<ActivityDto>>>
         {
-            public PagingParams Params { get; set; }
+            public ActivityParams Params { get; set; }
         }
         
         public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
@@ -34,14 +35,25 @@ namespace Application.Activities
 
             public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var queryable = _context.Activities
-                    .OrderBy(d => d.Date)
+                var query = _context.Activities
+                    .Where(a => a.Date >= request.Params.StartDate)
+                    .OrderBy(a => a.Date)
                     .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
                         new {currentUserName = _userAccessor.GetUserName()})
                     .AsQueryable();
-                
+
+                if (request.Params.IsHost)
+                {
+                    query = query.Where(x => x.HostUserName == _userAccessor.GetUserName());
+                }
+                else if (request.Params.IsGoing && !request.Params.IsHost)
+                {
+                    query = query.Where(x => x.Attendees.Any(a => 
+                        a.UserName == _userAccessor.GetUserName()));
+                }
+
                 return Result<PagedList<ActivityDto>>.Success(
-                    await PagedList<ActivityDto>.CreateAsync(queryable, request.Params.PageNumber, 
+                    await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber, 
                         request.Params.PageSize)
                 );
             }
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from './ActivityFilters';
 import { PagingParams } from '../../../app/models/pagination';
 import InfiniteScroll from "react-infinite-scroller";
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
@@ -23,19 +24,24 @@ export default observer(function ActivityDashboard() {
         if (activityRegistry.size <= 1) loadActivities();
     }, [loadActivities, activityRegistry.size]);
 
-    if (loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...'/>
-
     return (
         <Grid>
             <Grid.Column width="10">
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={handleGetNext}
-                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-                    initialLoad={false}
-                >
-                    <ActivityList/>
-                </InfiniteScroll>
+                {loadingInitial && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        initialLoad={false}
+                    >
+                        <ActivityList/>
+                    </InfiniteScroll>
+                )}
             </Grid.Column>
             <Grid.Column width="6">
                 <ActivityFilters />
